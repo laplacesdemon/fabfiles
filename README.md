58d6a7cfd0f89368b0741428a7a747605a7be816
Suleyman Melikoglu <suleymanmelikoglu@gmail.com>
2012-10-31 23:21:09 +0200

    apache related code is added, organization is changed

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ My Fabric scripts for deployment.
 
 Fabric is a great way of running procedures on remote server, as well as your local machine. Yet powerful since they are merely python scripts. I find it way easier and maintanable then writing bash scripts. 
 
-Repo is organized by different scenerios. i.e deployment for gunicorn or apache. 
-
 These scripts asumes that you use Mac OSX as your development environment and a debian based linux distro (i.e. ubuntu) as your server environment.  
+
+I couldn't decide on the organization of my fabfiles yet. There are different server environments and needs for each project. 
+Thus my first reaction is to create a fabfile for each environment, along with a base fabfile class to maintain them. 
+But since the scripts do trivial stuff (for now), I decided to put them in one place, organize them in a neat naming convention (underscore server name) 
---
+++ fabfile.py
@@ -0,0 +1,149 @@
+from fabric.api import *
+from fabric.contrib.console import confirm
+
+# enter the host names to run remotely
+# it can be more than one host
+# use ssh style. i.e solomon@ninja
+env.hosts = ['inanlarmobile.com']
+
+# project related settings
+project_name = 'inanlar'
+remote_code_dir = '/srv/www/inanlar'
+git_clone = 'solomon@46.19.36.179:/home/solomon/inanlar_insaat_web.git'
+virtual_env_name = 'venv'
+
+# gunicorn details
+remote_gunicorn_pid = '/tmp/inanlar.pid'
+
+################################################
+# Local commands                               #
+################################################
+
+
+def test():
+    with settings(warn_only=True):
+        result = local('python manage.py test', capture=True)
+    if result.failed and not confirm("Test failed, Continue anyway?"):
+        abort("Aborting at user request.")
+
+
+def commit():
+    local("git add -p && git commit")
+
+
+def push():
+    local("git push")
+
+
+def prepare_deploy():
+    """tests, commits and pushes the project"""
+    test()
+    commit()
+    push()
+
+
+################################################
+# Remote commands                              #
+################################################
+
+
+def clone():
+    with settings(warn_only=True):
+        # Check if the codes are cloned
+        if run("test -d %s" % remote_code_dir).failed:
+            run("git clone %s %s" % (git_clone, remote_code_dir))
+
+
+################################################
+# Apache commands                              #
+################################################
+
+
+def restart_apache():
+    """
+    restarts the remote apache server
+    """
+    # Restart apache
+    sudo('/etc/init.d/apache2 restart')
+
+
+def deploy_apache():
+    """
+    deploys the project to the remote server
+    """
+    clone()
+
+    with cd(remote_code_dir):
+        # Check if there is virtualenv
+        if run("test -d %s" % virtual_env_name).failed:
+            run("virtualenv %s --distribute" % virtual_env_name)
+        run('git pull')
+
+        with prefix('source %s/bin/activate' % virtual_env_name):
+            # Install pip requirements
+            run('pip install -r requirements.txt')
+
+            # Run django commands
+            #run('python manage.py syncdb')
+
+    # Restart apache
+    restart_apache()
+
+
+################################################
+# Gunicorn commands                            #
+################################################
+
+
+def restart_gunicorn():
+    """
+    restarts the gunicorn server
+    """
+    # I started the server before 'python manage.py run_gunicorn domainname.com:8000 -p /tmp/inanlar.pid -D'
+    # Restart server
+    run('kill -HUP `cat %s`' % remote_gunicorn_pid)
+
+
+def deploy_gunicorn():
+    """
+    deploys the project to the remote gunicorn server
+    """
+    clone()
+
+    with cd(remote_code_dir):
+        # Check if there is virtualenv
+        if run("test -d %s" % virtual_env_name).failed:
+            run("virtualenv %s --distribute" % virtual_env_name)
+        run('git pull')
+
+        with prefix('source %s/bin/activate' % virtual_env_name):
+            # Install pip requirements
+            run('pip install -r requirements.txt')
+
+    restart_gunicorn()
+
+
+################################################
+# Db Migration commands                        #
+################################################
+
+
+def migrate(app_name=None):
+    """
+    issues a migration using SAUTH db migration tool
+    """
+
+    if not app_name:
+        abort("Please provide an app to migrate, i.e. fab migrate:app_name=django_myapp")
+
+    with cd(remote_code_dir):
+        # Check if there is virtualenv
+        if run("test -d %s" % virtual_env_name).failed:
+            run("virtualenv %s --distribute" % virtual_env_name)
+        run('git pull')
+
+        with prefix('source %s/bin/activate' % virtual_env_name):
+            # Run django commands
+            run('python %s/manage.py migrate %s' % (project_name, app_name))
+
+    restart_apache()
